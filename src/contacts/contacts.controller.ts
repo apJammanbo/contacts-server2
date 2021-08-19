130426c82f114150624712ef5efbdb584cb25a52
apJammanbo <apJammanbo@gmail.com>
2021-08-19 16:41:54 +0900

    완료

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,32 +1,47 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ContactsService } from './contacts.service';
+import { Contact } from '.prisma/client';
+import { Prisma } from '@prisma/client';
 
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
   @Get()
-  getAll() {
-    return 'get all';
+  async getAll(): Promise<Contact[]> {
+    return await this.contactsService.getAll();
   }
 
   @Get('/:id')
-  get(@Param('id') id) {
-    return `get id: ${id}`;
+  async get(@Param('id') id): Promise<Contact> {
+    return await this.contactsService.get(Number(id));
   }
 
   @Post()
-  create() {
-    return `create`;
+  async create(
+    @Body() createConatactInput: Prisma.ContactCreateInput,
+  ): Promise<Contact> {
+    return await this.contactsService.create(createConatactInput);
   }
 
-  @Delete()
-  delete() {
-    return `delete`;
+  @Delete('/:id')
+  delete(@Param('id') id) {
+    return this.contactsService.delete(Number(id));
   }
 
-  @Put()
-  update() {
-    return `update`;
+  @Put('/:id')
+  update(
+    @Param('id') id,
+    @Body() contactUpdateInput: Prisma.ContactUpdateInput,
+  ) {
+    return this.contactsService.update(Number(id), contactUpdateInput);
   }
 }
